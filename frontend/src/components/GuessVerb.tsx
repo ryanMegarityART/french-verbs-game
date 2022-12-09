c10e526589d6b1a99ed872d182ad66bf29cad309
ryanMegarityART <ryan.megarity@pshealthgroup.com>
2022-12-09 16:49:55 +0000

    add basic translation check

--- frontend/src/components/GuessVerb.tsx
+++ frontend/src/components/GuessVerb.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { DisplayTranslation } from "./DisplayTranslation";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const ENDPOINT = "http://localhost:4000";
 
@@ -14,12 +14,25 @@ export const GuessVerb = () => {
   const [verb, setVerb] = useState<string>("");
   const [translation, setTranslation] = useState<string>("");
 
+  const [guess, setGuess] = useState<string>("");
+
   const getVerbAndTranslation = async () => {
     const resp = await fetch(ENDPOINT + "/verb");
     const respJSON: VerbResponse = await resp.json();
 
     setVerb(respJSON.verb);
     setTranslation(respJSON.translation);
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("answer: ", translation, "guess: ", guess);
+    if (guess === translation) {
+      alert("correct!");
+      getVerbAndTranslation();
+    } else {
+      alert("incorect, try again");
+    }
   };
 
   useEffect(() => {
@@ -32,10 +45,14 @@ export const GuessVerb = () => {
         <div className="guess">
           <h2>{verb}</h2>
           <DisplayTranslation translation={translation} />
-          <Form>
+          <Form onSubmit={onSubmit}>
             <Form.Group className="mb-3" controlId="formBasicGuess">
               <Form.Label>Enter Guess</Form.Label>
-              <Form.Control type="text" placeholder="_ _ _ _" />
+              <Form.Control
+                type="text"
+                placeholder="_ _ _ _"
+                onChange={(e) => setGuess(e.target.value)}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
